1f958b96ffa75ec7b574f90efd66be03d32fabbf
Renata Liu <renata_liu@outlook.com>
2025-06-02 23:13:07 -0700

    Initial Commit

---
+++ framed/framedApp.swift
@@ -0,0 +1,17 @@
+//
+//  framedApp.swift
+//  framed
+//
+//  Created by Renata Liu on 2025-06-02.
+//
+
+import SwiftUI
+
+@main
+struct framedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
